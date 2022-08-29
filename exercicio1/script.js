c1e30149988b2b4cd80c33a72b1e95ec96690b2e
CamiFranca <camillaartc@gmail.com>
2022-08-29 20:25:03 -0300

    três exercicios feitos

---
+++ exercicio1/script.js
@@ -0,0 +1,16 @@
+let nome 
+let  idade 
+console.log(typeof nome, typeof idade)
+//typeof diz qual o tipo da variável
+
+//porque eu não declarei o tipo
+nome = prompt('Qual o seu nome?')
+// O prompt abre uma janela na página do chrome
+
+idade = Number (prompt('Qual a sua idade?'))
+ // passou de string para number
+
+console.log(typeof nome, typeof idade)
+//porque eu adicionei um valor do tipo string
+
+console.log(`Olá ${nome}, você tem ${idade} anos.`)
